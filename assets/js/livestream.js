46ae99d609c5f1fbf9c44ac8a96451665c60d206
Danielle <ninasegurar@icloud.com>
2026-08-20 10:37:31 -0400

    Update livestream.js

--- assets/js/livestream.js
+++ assets/js/livestream.js
@@ -29,10 +29,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var target = wedding.isoDateTime ? new Date(wedding.isoDateTime) : null;
 
+  var expandIcon = '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3"></path><path d="M21 8V5a2 2 0 0 0-2-2h-3"></path><path d="M3 16v3a2 2 0 0 0 2 2h3"></path><path d="M16 21h3a2 2 0 0 0 2-2v-3"></path></svg>';
+  var shrinkIcon = '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3v3a2 2 0 0 1-2 2H3"></path><path d="M21 8h-3a2 2 0 0 1-2-2V3"></path><path d="M3 16h3a2 2 0 0 1 2 2v3"></path><path d="M16 21v-3a2 2 0 0 1 2-2h3"></path></svg>';
+
+  function isFullscreen() {
+    return !!(document.fullscreenElement || document.webkitFullscreenElement);
+  }
+
+  function toggleFullscreen() {
+    if (!videoFrame) return;
+    if (isFullscreen()) {
+      if (document.exitFullscreen) document.exitFullscreen();
+      else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    } else {
+      if (videoFrame.requestFullscreen) videoFrame.requestFullscreen();
+      else if (videoFrame.webkitRequestFullscreen) videoFrame.webkitRequestFullscreen();
+    }
+  }
+
   function renderEmbed() {
     if (!videoFrame) return;
     if (live.embedUrl) {
-      videoFrame.innerHTML = '<iframe src="' + live.embedUrl + '" title="Wedding livestream" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>';
+      videoFrame.innerHTML =
+        '<iframe src="' + live.embedUrl + '" title="Wedding livestream" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>' +
+        '<button type="button" class="fullscreen-btn" aria-label="View fullscreen">' + expandIcon + '</button>';
+
+      var fsBtn = videoFrame.querySelector(".fullscreen-btn");
+      if (fsBtn) {
+        fsBtn.addEventListener("click", toggleFullscreen);
+      }
+      ["fullscreenchange", "webkitfullscreenchange"].forEach(function (evt) {
+        document.addEventListener(evt, function () {
+          if (!fsBtn) return;
+          fsBtn.innerHTML = isFullscreen() ? shrinkIcon : expandIcon;
+          fsBtn.setAttribute("aria-label", isFullscreen() ? "Exit fullscreen" : "View fullscreen");
+        });
+      });
     } else {
       videoFrame.innerHTML =
         '<div class="video-placeholder">The livestream will appear here once we go live.<br>Check back at the ceremony start time!</div>';
